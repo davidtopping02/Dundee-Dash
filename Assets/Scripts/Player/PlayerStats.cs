2d66654eb58f92bc9c8babe7cf2ce1f8102adec4
David Topping <71976336+davidtopping02@users.noreply.github.com>
2023-04-11 19:09:29 +0100

    this works

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int health;
+    private int currentScore;
+    private int highScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 0;
+        currentScore = 0;
+
+        //implement this functionality
+        highScore = 0;
+    }
+
+    public void setHealth(int x)
+    {
+        health += x;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void setCurrentScore(int x)
+    {
+        currentScore += x;
+    }
+
+    public int getCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void setHighScore(int x)
+    {
+        highScore = x;
+    }
+}
